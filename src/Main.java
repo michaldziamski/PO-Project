ec5495507b9e84978423d11760464f4517c5a080
michaldziamski <mdziamski02@gmail.com>
2023-05-22 16:37:07 +0200

    test commit

--- src/Main.java
+++ src/Main.java
@@ -11,7 +11,7 @@ import java.util.ArrayList;
 
 public class Main {
     public static void main(String[] args) {
-
+        //da da daro
 
         Adult adult = new Adult("Daniel");
         Child child = new Child("John");
