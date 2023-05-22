29cc4feb201e44a0786d4ad4498d76395048687d
michaldziamski <mdziamski02@gmail.com>
2023-05-22 16:48:24 +0200

    test

--- src/Main.java
+++ src/Main.java
@@ -12,6 +12,7 @@ import java.util.ArrayList;
 public class Main {
     public static void main(String[] args) {
         //da da daro
+        //csonfipsnfpsof
 
         Adult adult = new Adult("Daniel");
         Child child = new Child("John");
